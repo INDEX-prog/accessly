c35e33f0a5e87e42be2e76dd69235719fd59ea79
INDEX-prog <tbertojo@ind-ex.fr>
2026-04-27 18:35:05 +0200

    feat: initial project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import FunnelDashboard from "@/components/FunnelDashboard";
+
+export default function DashboardPage() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <FunnelDashboard />
+    </main>
+  );
+}
